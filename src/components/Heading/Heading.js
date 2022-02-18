d8651d873d8d4d6cece83af31a515be7569f6ea2
MD.Nazrul <nazrul.com200@gmail.com>
2022-02-19 04:56:23 +0600

    new one

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,9 +1,9 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import Scrolling from '../Scrolling/Scrolling';
 import Typicals from '../Typical/Typicals';
-import './Heading.css'
 import React, { useState, useEffect, useRef } from 'react'
 import BIRDS from 'vanta/dist/vanta.birds.min'
+import './Heading.css'
 const Heading = () => {
     const [vantaEffect, setVantaEffect] = useState(0)
     const myRef = useRef(null)
@@ -24,7 +24,7 @@ const Heading = () => {
              <Row>
                 <Col md={6} sm={12} xs={12} lg={6}>
                 <div className="headerimg">
-                    <img width="100%" src="https://i.ibb.co/TByccHc/Nazrul.jpg" alt="" />
+                    <img src="https://i.ibb.co/TByccHc/Nazrul.jpg" alt="" />
                 </div>
                 </Col>
                 <Col  md={6} sm={12} xs={12} lg={6}>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import About from '../About/About';
+import './Home.css'
 import Card from '../Card/Card';
 import Contact from '../contact/Contact';
 import Headers from '../header/Headers';
 import Heading from '../Heading/Heading';
 const Home = () => {
     return (
-        <div id="home">
+        <div className='allBody' id="home">
             <Headers></Headers>
             <Heading></Heading>
             <About></About>
